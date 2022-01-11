709236f4a7f9ed333a42b8747acd79368d46c39a
Trygve Wastvedt <trygve.wastvedt@gmail.com>
2022-01-11 02:00:59 +0000

    Start settings page.

---
+++ src/client/components/SettingsPage.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-container>
+    <v-data-table :items="state.varieties" :headers="varietyHeaders">
+    </v-data-table>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Store from "../Store";
+import type { DataTableHeader } from "vuetify";
+
+@Component({})
+export default class SettingsPage extends Vue {
+  state = Store.state;
+
+  varietyHeaders: DataTableHeader[] = [
+    {
+      text: "Name",
+      value: "name",
+    },
+    {
+      text: "Color",
+      value: "color",
+    },
+    {
+      text: "Family",
+      value: "familyId",
+    },
+    {
+      text: "Plants",
+      value: "plants.length",
+    },
+  ];
+
+  async mounted(): Promise<void> {}
+}
+</script>
